bd703197e8fada83e377663e89a72020ecb3d069
Abdourahmane <daffabdourahmane50@gmail.com>
2024-12-18 20:04:37 +0000

    modification du fichier index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 // Fonction principale pour afficher le message
 function main() {
-    echo "Hello, GitHub!";
+    echo "Hey GitHub, let's code!";
    }
    // Appel de la fonction principale
    main(); 
